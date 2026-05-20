b058b8d468e429c9a66e80697f608c791929c3b8
Mattias Jönsson <mattias.j.jonsson@hotmail.com>
2026-05-20 20:54:41 +0200

    commit before starting to connecting new structre to old

--- IGarage.cs
+++ IGarage.cs
@@ -3,12 +3,17 @@ using GarageBuilder.Vehicles;
 
 namespace GarageBuilder
 {
-    internal interface IGarage<T> : IEnumerable<T> where T : Vehicle
+    internal interface IGarage<T> : IGarage, IEnumerable<T> where T : Vehicle
     {
         public string Name {get;}
         public void AddVehicle(T vehicle);
         public void RemoveVehicle(int index); // borde vara vehicle:id!?
         public T[] FindVehicle(string type, string id, string coulour, int weight);
 
+    }
+
+    internal interface IGarage
+    {
+        
     }
 }
--- Manager.cs
+++ Manager.cs
@@ -1,11 +1,13 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using GarageBuilder.UI;
+using GarageBuilder.Vehicles;
 
 namespace GarageBuilder
 {
     internal class Manager
     {
-        private List<IGarage<T>> allGarages;
+        private List<IGarage> allGarages = new List<Garage<T>>();
         
         public void runApp(IUserInterface userIf)
         {
